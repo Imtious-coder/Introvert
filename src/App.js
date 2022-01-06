f924d7d012d8fa6889888ff2863ca31cfae58d1c
Imtious-coder <iarafat386@gmail.com>
2022-01-06 19:32:04 +0600

    Project Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Image from './My.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="main">
+            <h2>Hi! buddy</h2>
+            {/* FORM */}
+            <form id="form" action="https://formspree.io/f/mpzbwkee" method="POST">
+                <a href='https://www.facebook.com/imtious.midul/'><img src={Image} alt="" /></a>
+                <h3>Imtious Islam</h3>
+                {/* MESSAGE */}
+                <textarea placeholder='Message | Suggestion | Advise . . .' name="Message"/>
+                {/* SUBMIT BUTTON */}
+                <button type="submit">Send</button>
+            </form>
+      </div>
+
   );
 }
 
